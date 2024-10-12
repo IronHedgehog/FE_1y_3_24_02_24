b496815043398ba9372b0f9f18f6c3b2e7292254
Artem Faust <91615835+IronHedgehog@users.noreply.github.com>
2024-10-12 12:42:35 +0300

    a

---
+++ js.js
@@ -0,0 +1,18 @@
+function getRandomColor() {
+  const letters = "0123456789ABCDEF";
+  let color = "#";
+  for (let i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random() * 16)];
+  }
+  return color;
+}
+
+function changeBackgroundColor() {
+  const newColor = getRandomColor();
+  document.body.style.backgroundColor = newColor;
+  document.getElementById("currentColor").textContent = newColor;
+}
+
+changeBackgroundColor();
+
+setInterval(changeBackgroundColor, 15000);
